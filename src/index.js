29615f40bdb8ad6bb7cb99d2b04f23153a280bbb
Tawonga Jongwe <reginaldjongwe@gmail.com>
2021-12-01 10:51:34 +0000

    books

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './Book Project/app';
+import App from './app';
 import  "./bookProject.css";
 
 import {BrowserRouter as Router } from 'react-router-dom'
